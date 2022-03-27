2d93edaa61fefab856e01998587fd81f7245c914
kunal_cpp <kunaliesc@gmail.com>
2022-03-27 17:36:18 +0530

    Update Subsets.cpp

--- Subsets.cpp
+++ Subsets.cpp
@@ -16,30 +16,24 @@ Output: [[],[0]]
  
 
 Constraints:
-1 <= nums.length <= 10
+1 <= nums.length <= 10   // n can be max upto 20
 -10 <= nums[i] <= 10
 All the numbers of nums are unique.
 */
 
-class Solution {
-public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        
-        vector<vector<int>> ans;
-        
-        int n = nums.size();
-        for(int i = 0; i < (1<<n); i++){
-            int idx = 0;
-            vector<int> v;
-            for(int num = i; num > 0; num = num>>1){
-                if((num & 1) == 1) v.push_back(nums[idx]);
-                idx++;
-            }
-            ans.push_back(v);
+// TC: (2^n)*n
+vector<vector<int>> subsets(vector<int>& nums) {
+    int n = nums.size();
+
+    vector<vector<int>> ans;
+    for(int i = 0; i < (1<<n); i++){
+        vector<int> temp;
+        for(int j = 0; j < n; j++){
+            if((i>>j) & 1) temp.push_back(nums[j]);
         }
-        
-    return ans;
-    }
-};
+        ans.push_back(temp);
+    }   
+return ans;
+}
 
 
